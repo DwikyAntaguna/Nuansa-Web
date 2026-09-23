0b8ef4039bff4d92d60dc4c2c47581d8d4c07f27
dwiky antaguna <dwikyantaguna@MacBook-Air-dwiky.local>
2026-09-23 12:46:56 +0800

    Mengunggah seluruh file utama project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                navy: '#0F172A', 
+                electric: '#2563EB',
+                emerald: '#10B981', 
+            }
+        },
+    },
+    plugins: [],
+}
